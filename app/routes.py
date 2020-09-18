8d0b1a2a0e6d56f78f672d906ec424847e879b2d
Vadim <delfvad@gmail.com>
2020-09-18 12:22:24 +0300

    Login netloc check has been added

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,14 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_users, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Эльдар Рязанов'}
     posts = [{
@@ -37,7 +39,10 @@ def login():
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-		return redirect(url_for('index'))
+		next_page = request.args.get('next')
+		if not next_page or url_pars(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
