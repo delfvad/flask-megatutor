d8cb437b110b106793234805fe4216f27146f101
Vadim <delfvad@gmail.com>
2020-09-18 11:31:17 +0300

    DB and login base function has been added

--- app/routes.py
+++ app/routes.py
@@ -2,33 +2,47 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_users, logout_user
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'Эльдар Рязанов'}
-	posts = [{
-		'author': {'username': 'John'},
-		'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		},
-		{
-			'author': {'username': 'Ипполит'},
-			'body': 'Какая гадость эта ваша заливная рыба!!'
-		}
-	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
-	
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [{
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+        {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    },
+        {
+        'author': {'username': 'Ипполит'},
+        'body': 'Какая гадость эта ваша заливная рыба!!'
+    }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
 		return redirect(url_for('index'))
-	return render_template('login.html',title='Sign In',form=form)
-	
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
